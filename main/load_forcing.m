44a4a202d084cf1baabc3209021f22ed150ee449
jguiet <33251089+jguiet@users.noreply.github.com>
2017-12-04 16:40:24 +0100

    Change forcing name
    Economical.mat -> Economy.mat
    Ecological.mat -> Ecology.mat
    ical -> y

--- main/load_forcing.m
+++ main/load_forcing.m
@@ -1,43 +1,43 @@
 %**************************************************************************************************************
 % FUNCTION load_forcing.m
 % Load forcings preprocessed by "preprocess.m" for the simulation :
-% Ecological.mat
-% Economical.mat
+% Ecology.mat
+% Economy.mat
 %**************************************************************************************************************
-function forcing = load_forcing(boats,forcing_ecological,forcing_economical)
+function forcing = load_forcing(boats,forcing_ecology,forcing_economy)
 
 %---------------------------------
-% Forcing ecological:
-if exist(forcing_ecological,'file')
-    load(forcing_ecological);
+% Forcing ecology:
+if exist(forcing_ecology,'file')
+    load(forcing_ecology);
 else
-    disp('Hum, double-check the path for ecological forcing:')
-    disp(forcing_ecological)
+    disp('Hum, double-check the path for ecology forcing:')
+    disp(forcing_ecology)
 end
-forcing.mask=repmat(Ecological.mask,[1 1 size(Ecological.npp,3)]);
+forcing.mask=repmat(Ecology.mask,[1 1 size(Ecology.npp,3)]);
 forcing.nlat=size(forcing.mask,1);
 forcing.nlon=size(forcing.mask,2);
-forcing.npp=Ecological.npp;
+forcing.npp=Ecology.npp;
 forcing.npp(find(forcing.mask==1))=NaN;
-forcing.npp_ed=Ecological.npp_ed;
+forcing.npp_ed=Ecology.npp_ed;
 forcing.npp_ed(find(forcing.mask==1))=NaN;
-forcing.temperature=Ecological.temperature;
-forcing.temperature_K=Ecological.temperature+boats.param.conversion.C_2_K;
-forcing.surf=Ecological.surface;
+forcing.temperature=Ecology.temperature;
+forcing.temperature_K=Ecology.temperature+boats.param.conversion.C_2_K;
+forcing.surf=Ecology.surface;
 
 %--------------------------------- 
-% Forcing economical
+% Forcing economy
 if strcmp(boats.param.main.sim_type,'h')
-    if exist(forcing_economical,'file')
-        load(forcing_economical);
+    if exist(forcing_economy,'file')
+        load(forcing_economy);
     else
-        disp('Hum, double-check the path for economical forcing:')
-        disp(forcing_economical)
+        disp('Hum, double-check the path for economy forcing:')
+        disp(forcing_economy)
     end
-    load(forcing_economical)
-    forcing.price=Economical.price;
-    forcing.cost=Economical.cost;
-    forcing.catchability=Economical.catchability;
+    load(forcing_economy)
+    forcing.price=Economy.price;
+    forcing.cost=Economy.cost;
+    forcing.catchability=Economy.catchability;
 end
  
 %**************************************************************************************************************
